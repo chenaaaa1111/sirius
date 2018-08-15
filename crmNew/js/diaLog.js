5b59c67541f0e180e3e18f8f0bb03f7ef7b0d1ef
chenaaaa1111 <710898849@qq.com>
2018-08-15 20:21:09 +0800

    add newProject

---
+++ crmNew/js/diaLog.js
@@ -0,0 +1,14 @@
+   $(function(){
+   		    $("#add").click(function(){
+       			 $(".mask").toggle();
+   			 })
+		    $(".closeMask").click(function(){
+		        $(".mask").toggle();
+		    })
+
+		    $(".togglegan").click(function(){
+		    	$('.toggle').toggle();
+		    })
+   })
+
+
